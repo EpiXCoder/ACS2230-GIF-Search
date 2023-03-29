cb001d20797ac5b2f4d09483105a8d7dbde0902a
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-03-29 17:56:38 -0400

    added search form

--- app.js
+++ app.js
@@ -23,11 +23,9 @@ app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-})
+    console.log(req.query) // => "{ term: hey" }
+    res.render('home')
+  })
 
 app.get('/greetings/:name', (req, res) => {
     // grab the name from the path provided
@@ -35,7 +33,7 @@ app.get('/greetings/:name', (req, res) => {
     // render the greetings view, passing along the name
     res.render('greetings', { name });
   })
-  
+
 // Start Server
 
 //tells the app to log a message on port 3000. Once we start the server, we should see that message in the terminal 
